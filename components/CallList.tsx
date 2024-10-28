7d3dbc57312c96ce71fbeab37370ab337e06db24
lantemir <temiros@mail.ru>
2024-10-28 20:10:28 +0500

    fix

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { Call, CallRecording } from '@stream-io/video-react-sdk';
-// import Meeting from '@/app/(root)/meeting/[id]/page';
 import { useGetCalls } from '@/hooks/useGetCalls'
 
 
@@ -82,9 +81,9 @@ const CallList = ({type}: {type: 'ended' | 'upcoming' | 'recordings'}) => {
 
   return (
     <div className='grid grid-cols-1 gap-5 xl:grid-cols-2'>
-        {calls && calls.length > 0 ? calls.map((meeting: Call | CallRecording) => (
+        {calls && calls.length > 0 ? calls.map((meeting) => (
             <MeetingCard
-            key={(meeting as Call)?.id}
+            key={'id' in meeting ? meeting.id : meeting.url}
             icon={
                 type === 'ended'
                     ? '/icons/previous.svg'
@@ -92,13 +91,21 @@ const CallList = ({type}: {type: 'ended' | 'upcoming' | 'recordings'}) => {
                         ? '/icons/upcoming.svg'
                         : '/icons/recordings.svg'
             }
-            title={(meeting as Call).state?.custom?.description?.substring(0, 26) || meeting?.filename?.substring(0, 20) || "Personal meeting"}
-            date={meeting.state?.startsAt.toLocaleString() || meeting.start_time.toLocaleString()}
+            title={'filename' in meeting
+                ? meeting.filename.substring(0, 20)
+                : (meeting as Call).state?.custom?.description?.substring(0, 26) || "Personal meeting"}
+            date={  'start_time' in meeting
+                ? meeting.start_time?.toLocaleString() || ""
+                : (meeting as Call).state?.startsAt?.toLocaleString() || ""}
             isPreviousMeeting={type === 'ended'}
             buttonIcon1={type === 'recordings' ? '/icons/play.svg' : undefined}
             buttonText={type === 'recordings' ? 'Play' : "Start"}
-            handleClick={type === 'recordings' ? () => router.push(`${meeting.url}`) : () => router.push(`/meeting/${meeting.id}`)}
-            link={type === 'recordings' ? meeting.url : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meeting.id}`}
+            handleClick={ type === 'recordings'
+                ? () => router.push(`${(meeting as CallRecording).url}`)
+                : () => router.push(`/meeting/${(meeting as Call).id}`)}
+            link={type === 'recordings'
+                ? (meeting as CallRecording).url
+                : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${(meeting as Call).id}`}
            
             />
         )) : (
